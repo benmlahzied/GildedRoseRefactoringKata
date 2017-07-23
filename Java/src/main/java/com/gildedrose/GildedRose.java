f5e0103e3da215ec79ec44ac87fa096a8317fe5d
Zied <benmlahzied@gmail.com>
2017-07-23 14:40:16 +0200

    [Refactoring] Extract methods with meaningful names, invert and join some conditions

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,68 +1,87 @@
 package com.gildedrose;
 
 class GildedRose {
-	
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+
+	private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 	private static final String AGED_BRIE = "Aged Brie";
 	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 	private static final int MAXIMUM_QUALITY = 50;
-	
-    Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	Item[] items;
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < MAXIMUM_QUALITY) {
-                    items[i].quality = items[i].quality + 1;
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-                    if (items[i].name.equals(BACKSTAGE)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < MAXIMUM_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+	public void updateQuality() {
+		for (Item item : items) {
+			updateItemQuality(item);
+		}
+	}
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < MAXIMUM_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+	private void updateItemQuality(Item item) {
+		if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE)) {
+			if (hasQualityLessThenMax(item)) {
+				incrementQuality(item);
 
-            if (!items[i].name.equals(SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+				if (item.name.equals(BACKSTAGE)) {
+					updateBackstageQuality(item);
+				}
+			}
+		} else {
+			if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+				decrementQuality(item);
+			}
+		}
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < MAXIMUM_QUALITY) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+		if (!item.name.equals(SULFURAS)) {
+			decrementSellIn(item);
+		}
+
+		if (hasExpired(item)) {
+			if (item.name.equals(AGED_BRIE)) {
+				if (hasQualityLessThenMax(item)) {
+					incrementQuality(item);
+				}
+			} else if (item.name.equals(BACKSTAGE)) {
+				item.quality = 0;
+			} else if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+				decrementQuality(item);
+			}
+		}
+	}
+
+	private boolean hasExpired(Item item) {
+		return item.sellIn < 0;
+	}
+
+	private void updateBackstageQuality(Item item) {
+		if (item.sellIn < 11) {
+			if (hasQualityLessThenMax(item)) {
+				incrementQuality(item);
+			}
+		}
+
+		if (item.sellIn < 6) {
+			if (hasQualityLessThenMax(item)) {
+				incrementQuality(item);
+			}
+		}
+	}
+
+	private int decrementQuality(Item item) {
+		return item.quality = item.quality - 1;
+	}
+
+	private int decrementSellIn(Item item) {
+		return item.sellIn = item.sellIn - 1;
+	}
+
+	private int incrementQuality(Item item) {
+		return item.quality = item.quality + 1;
+	}
+
+	private boolean hasQualityLessThenMax(Item item) {
+		return item.quality < MAXIMUM_QUALITY;
+	}
 }
